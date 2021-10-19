332ed6fb78ac3c48cb51c5370f86b5389932f973
hatetavian <tavianjdawson123@gmail.com>
2021-10-19 18:40:51 -0500

    player-name needed

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,15 @@ var randomNumber = function(min, max) {
 
     return value;
 };
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null ) {
+        name = prompt("What is your robot's name?");
+    }
+}
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -126,6 +132,7 @@ var fight = function (enemy) {
              if (playerInfo.health > 0) {
                  // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
                  window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+                 // debugger;
                  // pick new enemy to fight based on the index of the enemyNames array
                  var pickedEnemyObj = enemyInfo[i];
                  // reset enemyHealth before starting new fight
