d817dc592601887cef24f5484cfb2048cde6e9c2
hatetavian <tavianjdawson123@gmail.com>
2021-10-19 19:30:31 -0500

    shop into integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -179,23 +179,20 @@ var fight = function (enemy) {
          }
      }
      var shop = function() {
-         var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+         var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 to LEAVE shop.");
+         shopOptionPrompt = parseInt(shopOptionPrompt);
          switch (shopOptionPrompt) {
-             case "refill":
-            case "REFILL":
+             case 1:
                 playerInfo.refillHealth();
                  break;
-                case "upgrade":
-                case "UPGRADE":
+                case 2:
                     playerInfo.upgradeAttack();
                     break;
-                case "leave":
-                case "LEAVE":
+                case 3:
                 window.alert("leave the shop.");
                 break;
                 default:
                     window.alert('You did not pick a valid option. Try again.');
-
                     shop();
                     break;
          }
